755bad5ae95d601324af5d8857d69239804e4405
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-08-22 22:40:47 +0530

    Set ith bit

--- Bit-Manipulation/setIthbit.cpp
+++ Bit-Manipulation/setIthbit.cpp
@@ -9,11 +9,18 @@ int getIthBit(int n, int i)
 }
 
 // Setting ith bit of a number
+int setIthBit(int n, int i)
+{
+    int mask = (1 << i);
+    return n | mask;
+}
 
 int main()
 {
     int n, i;
     cin >> n >> i;
     cout << getIthBit(n, i);
+    cout << endl
+         << setIthBit(n, i);
     return 0;
 }
