75257b0f668c332548fb3a3be0c10310f2615229
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-09-11 00:05:45 +0530

    Quick sort algo

---
+++ Sorting & Searching/quickSort.cpp
@@ -0,0 +1,45 @@
+/*
+Quick sort implementation
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int> &arr, int s, int e)
+{
+    int pivot = arr[e];
+    int i = s - 1;
+    for (int j = s; j < e; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[e]);
+    return i + 1;
+}
+
+void quickSort(vector<int> &arr, int s, int e)
+{
+    if (s >= e)
+    {
+        return;
+    }
+    int p = partition(arr, s, e);
+    quickSort(arr, s, p - 1);
+    quickSort(arr, p + 1, e);
+}
+
+int main()
+{
+    vector<int> arr = {5, 4, 3, 2, 1};
+    quickSort(arr, 0, arr.size() - 1);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
