45110af0b37ca97bb33d3a34d6cf587a524a0e21
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-10-08 17:01:08 +0530

    Climbing Ladder Code

--- Recursion/climbingLadder.cpp
+++ Recursion/climbingLadder.cpp
@@ -8,3 +8,32 @@ Find the number of
 ways to climb the
 ladder.
 */
+
+#include <iostream>
+using namespace std;
+
+int climbingLadder(int n)
+{
+    int c1, c2, c3;
+    // To prevent the value of n going negative
+    if (n < 0)
+    {
+        return 0;
+    }
+    // Base Case
+    if (n == 0)
+    {
+        return 1;
+    }
+    // Recursive Case
+    return climbingLadder(n - 3) + climbingLadder(n - 2) + climbingLadder(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a positive whole number" << endl;
+    cin >> n;
+    cout << "No. of ways to reach the end of the ladder of " << n << " steps is " << climbingLadder(n);
+    return 0;
+}
