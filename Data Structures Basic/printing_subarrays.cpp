25a217247b75f5c3c93b592c6b7e6918759fce41
Anurag Negi <an6265081@gmail.com>
2025-03-29 12:08:33 +0530

    Printing Subarray

---
+++ Data Structures Basic/printing_subarrays.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int printSubarrays(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << ",";
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printSubarrays(arr, n);
+}
